52463a81ecc2d12c738bc7325a1df336b37120a6
Joe Dowswell <joe@radix2design.co.uk>
2012-09-24 15:21:10 +0100

    [M95256] SPI EEPROM driver - read and write abitrary length buffers - requires SPI0.h

---
+++ lib/M95256.c
@@ -0,0 +1,58 @@
+/*
+	A driver for M95256-WMN6P SPI EEPROM
+*/
+#include "platform.h"
+#include "SPI0.h"
+#include "M95256.h"
+
+enum M95256_INSTRUCTIONS {
+	// write enable/disable
+	e_M95256_WREN = 0x06, e_M95256_WRDI = 0x04,
+	// read/write status register
+	e_M95256_RDSR = 0x05, e_M95256_WRSR = 0x01,
+	// read/write memory
+	e_M95256_READ = 0x03, e_M95256_WRITE = 0x02
+};
+
+void M95256_read(const SPI0_device device, short address, unsigned char *const buffer, unsigned char length)
+{
+unsigned char i;
+	length %= M95256_PAGE_SIZE;
+	SPI0_select(device);
+	SPI0_byte(e_M95256_READ);
+	SPI0_byte(address >> 8);
+	SPI0_byte(address);
+	for (i = 0; i < length; i++)
+	{
+		buffer[i] = SPI0_byte(0);
+	}
+	SPI0_select(0);
+}
+
+void M95256_write(const SPI0_device device, short address, unsigned char *const buffer, unsigned char length)
+{
+unsigned char i;
+	length %= M95256_PAGE_SIZE;
+	SPI0_select(device);
+	SPI0_byte(e_M95256_WREN);
+	SPI0_select(0);
+	i = 1; while (i++) NOP();
+	SPI0_select(device);
+	SPI0_byte(e_M95256_WRITE);
+	SPI0_byte(address >> 8);
+	SPI0_byte(address);
+	for (i = 0; i < length; i++)
+	{
+		SPI0_byte(buffer[i]);
+	}
+	SPI0_select(0);
+	i = 1; while (i++) NOP();
+	SPI0_select(device);
+	SPI0_byte(e_M95256_WRDI);
+	SPI0_select(0);
+	i = 1; while (i++) NOP();
+	SPI0_select(device);
+	SPI0_byte(e_M95256_RDSR);
+	while (SPI0_byte(0) & 3) ;
+	SPI0_select(0);
+}
---
+++ lib/M95256.h
@@ -0,0 +1,19 @@
+/*
+	A driver for M95256-WMN6P SPI EEPROM
+*/
+#ifndef __M95256_H
+#define __M95256_H
+
+#define M95256_PAGE_SIZE	64
+// the address of the first byte in the page contianing `address`
+#define M95256_PAGE(address)	((address) - ((address) % M95256_PAGE_SIZE))
+
+// read bytes into a buffer
+//		from `address` to `address + length`
+extern void M95256_read(const SPI0_device device, short address, unsigned char *const buffer, unsigned char length);
+
+// write bytes from a buffer
+//		from `address` to `address + (length % EE_PAGE_SIZE)`
+extern void M95256_write(const SPI0_device device, short address, unsigned char *const buffer, unsigned char length);
+
+#endif	// __M95256_H
