df43206d9796a29ec118253079b82c8fc4c731a3
Joe ST <joe@fbstj.net>
2013-03-29 14:27:36 +0000

    [buffers] add RING struct and _add and _get functions - RING_add(ring, item) adds item to the head of the ring - RING_get(item, add) fills item with the tail element

--- lib/buffers.h
+++ lib/buffers.h
@@ -8,15 +8,14 @@
 #define ring_count(len, head, tail)	(((len) + (head) - (tail)) % (len))
 #define ring_space(len, head, tail)	((len) - ring_count(len, head, tail))
 
-// typeless rings, with helper functions
-//#define T_RING(T, tag, len) struct tag { T buffer[len]; unsigned int head, tail; }
-#define RING_length(o)	(sizeof((o)->buffer) / sizeof((o)->buffer[0]))
-#define RING_count(o)	ring_count(RING_length(o), (o)->head, (o)->tail)
-#define RING_space(o)	ring_space(RING_length(o), (o)->head, (o)->tail)
-#define RING_clear(o)	(o)->head = (o)->tail = 0
+enum RING_STATE { RING_OK = 0, RING_EMPTY, RING_FULL, RING_ERROR };
 
-#define RING_put(o)		(o)->buffer[(o)->head++]
-#define RING_get(o)		(o)->buffer[(o)->tail++]
+// typeless ring, with helper functions
+struct RING { void * raw; unsigned int length, width, offset, count; };
+#define RING_clear(r)	(r)->offset = (r)->count = 0
+#define RING_head(r)	(((r)->offset + (r)->count) % (r)->length)
+enum RING_STATE RING_add(struct RING * const, const void * const);
+enum RING_STATE RING_get(void * const, struct RING * const);
 
 struct byte_ring { unsigned char buffer[256], head, tail; };
 
