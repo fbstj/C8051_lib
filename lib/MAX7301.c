ed16e1a81dc117a93f6d1f50b3da82d0bd6981d9
Joe Dowswell <joe@radix2design.co.uk>
2013-07-18 08:55:20 +0100

    [MAX7301] make IO_[sg]et into macros

--- lib/MAX7301.c
+++ lib/MAX7301.c
@@ -1,7 +1,6 @@
 /*
 	MAX7301 GPIO port expansion
 */
-#include "platform.h"
 #include "MAX7301.h"
 
 enum CMD {
@@ -15,39 +14,32 @@ enum CMD {
 	OUTPUT = 1, INPUT = 2, PULLUP = 3
 };
 
-static void IO_set(const struct MAX7301 * const self, const enum CMD command, const char value)
-{
-	self->word(((command & ~0x80) << 8) | value);
-}
-static char IO_get(const struct MAX7301 * const self, const enum CMD command)
-{
-	self->word((command | 0x80) << 8);
-	return self->word(0x8000);
-}
+#define IO_set(command, value)	self->word(((command & ~0x80) << 8) | value)
+#define IO_get(command)	(self->word((command | 0x80) << 8), self->word(0x8000))
 
 void MAX7301_init(const struct MAX7301 * const self)
 {
-	IO_set(self, CONF, 1);
+	IO_set(CONF, 1);
 }
 
 void MAX7301_conf(const struct MAX7301 * const self, char p, char conf)
 {
 int shift = (p % 4) << 1,	mask = 3 << shift;
-	IO_set(self, PC(p), (((conf & 3) << shift) & mask) | (IO_get(self, PC(p)) & ~mask));
+	IO_set(PC(p), (((conf & 3) << shift) & mask) | (IO_get(PC(p)) & ~mask));
 }
 
 char MAX7301_get(const struct MAX7301 * const self, char p)
 {
-	return IO_get(self, PORT(p));
+	return IO_get(PORT(p));
 }
 
 void MAX7301_set(const struct MAX7301 * const self, char p, char v)
 {
-	IO_set(self, PORT(p), v & 1);
+	IO_set(PORT(p), v & 1);
 }
 
 void MAX7301_change(const struct MAX7301 * const self, char p, char v, char m)
 {	// changes p thru p+7 to (v & m), leaving ~m alone
-	IO_set(self, P8(p % 32), (v & m) | (IO_get(self, P8(p % 32)) & ~m));
+	IO_set(P8(p % 32), (v & m) | (IO_get(P8(p % 32)) & ~m));
 }
 
