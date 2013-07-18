47e8aa18e629e2ef2981f9da69b3c020213b8d4e
Joe Dowswell <joe@radix2design.co.uk>
2013-07-18 08:43:47 +0100

    [M95256] SELECT macro - wraps actiions in chip select - use external sleep(i) function

--- lib/M95256.c
+++ lib/M95256.c
@@ -1,7 +1,6 @@
 /*
 	A driver for M95256-WMN6P SPI EEPROM
 */
-#include "platform.h"
 #include "M95256.h"
 
 enum M95256_INSTRUCTIONS {
@@ -13,45 +12,58 @@ enum M95256_INSTRUCTIONS {
 	e_M95256_READ = 0x03, e_M95256_WRITE = 0x02
 };
 
+#define SELECT(action) self->select(); action; self->deselect()
+
 void M95256_load( const struct M95256 * const self, const unsigned int address, unsigned char * const buffer, const unsigned char length)
 {
 unsigned char i;
+
 	length %= M95256_PAGE_SIZE;
-	self->select();
-	self->byte(e_M95256_READ);
-	self->byte(address >> 8);
-	self->byte(address);
-	for (i = 0; i < length; i++)
-	{
-		buffer[i] = self->byte(0);
-	}
-	self->deselect();
+
+	SELECT(
+		self->byte(e_M95256_READ);
+		self->byte(address >> 8);
+		self->byte(address);
+		for (i = 0; i < length; i++)
+		{
+			buffer[i] = self->byte(0);
+		}
+	);
 }
 
 void M95256_save(const struct M95256 * const self, const unsigned int address, const unsigned char * const buffer, unsigned char length)
 {
 unsigned char i;
+extern void sleep(volatile unsigned long);
+
 	length %= M95256_PAGE_SIZE;
-	self->select();
-	self->byte(e_M95256_WREN);
-	self->deselect();
-	i = 1; while (i++) NOP();
-	self->select();
-	self->byte(e_M95256_WRITE);
-	self->byte(address >> 8);
-	self->byte(address);
-	for (i = 0; i < length; i++)
-	{
-		self->byte(buffer[i]);
-	}
-	self->deselect();
-	i = 1; while (i++) NOP();
-	self->select();
-	self->byte(e_M95256_WRDI);
-	self->deselect();
-	i = 1; while (i++) NOP();
-	self->select();
-	self->byte(e_M95256_RDSR);
-	while (self->byte(0) & 3) ;
-	self->deselect();
+
+	SELECT(
+		self->byte(e_M95256_WREN)
+	);
+
+	sleep(256);
+
+	SELECT(
+		self->byte(e_M95256_WRITE);
+		self->byte(address >> 8);
+		self->byte(address);
+		for (i = 0; i < length; i++)
+		{
+			self->byte(buffer[i]);
+		}
+	);
+
+	sleep(256);
+
+	SELECT(
+		self->byte(e_M95256_WRDI)
+	);
+
+	sleep(256);
+
+	SELECT(
+		self->byte(e_M95256_RDSR);
+		while (self->byte(0) & 3) ;
+	);
 }
