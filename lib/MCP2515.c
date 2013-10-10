5d63ec6dd08cd9d42ce43119c7f6fa03cc9ab77a
Joe Dowswell <joe@radix2design.co.uk>
2013-10-10 11:08:32 +0100

    [MCP2515] support for extended/remote frames - use set/get macros - other improvements

--- lib/MCP2515.c
+++ lib/MCP2515.c
@@ -26,134 +26,161 @@ static void SPI_set(const struct MCP2515 * const self, unsigned char const addr,
 	);
 }
 
+#define set(n, v)	SPI_set(self, n, v)
+#define get(n)		SPI_get(self, n)
+
 void MCP2515_send(const struct MCP2515 * const self, const struct CAN_frame * const frame)
 {
-unsigned char TXB0CTRLval, cRet;
+unsigned char txctrl, cRet;
 	//Read TX Status
-	TXB0CTRLval = SPI_get(self, TXB0CTRL);
+	txctrl = get(TXB0CTRL);
 
 	//Has the previous message sent yet?  If not, resolve this before proceeding
-	if (TXB0CTRLval & TXREQ_SET)
+	if (txctrl & TXREQ_SET)
 	{	//Then the previous message has *not* sent yet...
 		do
 		{
 			//Was there an error?
-			if (TXB0CTRLval & 0x10)	// Last message started to transmit, but encountered an error condition
+			if (txctrl & 0x10)	// Last message started to transmit, but encountered an error condition
 				cRet = 0;
-			if (TXB0CTRLval & 0x20)	// Message lost arbitration while being sent (message was lost)
+			if (txctrl & 0x20)	// Message lost arbitration while being sent (message was lost)
 				cRet = -1;
 
 			if (cRet == 1)			// No error, the message is just busy sending itself - re-poll
-				TXB0CTRLval = SPI_get(self, TXB0CTRL);
+				txctrl = get(TXB0CTRL);
 			else					// Error of some kind: clear it
-				SPI_set(self, TXB0CTRL, 0);
+				set(txctrl, 0);
 		}
-		while ((TXB0CTRLval & TXREQ_SET) && (cRet == 1));
+		while ((txctrl & TXREQ_SET) && (cRet == 1));
 	}
 
-	SPI_set(self, TXB0SIDH, frame->ID >> 3);
-	SPI_set(self, TXB0SIDL, (frame->ID << 5) & 0xC0);
-	SPI_set(self, TXB0DLC, frame->Length & 0xF);
-	SPI_set(self, TXB0D0, frame->Data[0]); SPI_set(self, TXB0D1, frame->Data[1]);
-	SPI_set(self, TXB0D2, frame->Data[2]); SPI_set(self, TXB0D3, frame->Data[3]);
-	SPI_set(self, TXB0D4, frame->Data[4]); SPI_set(self, TXB0D5, frame->Data[5]);
-	SPI_set(self, TXB0D6, frame->Data[6]); SPI_set(self, TXB0D7, frame->Data[7]);
+	set(TXB0EID8, frame->ID >> 19);
+	set(TXB0EID0, frame->ID >> 11);
+	set(TXB0SIDH, frame->ID >> 3);
+	set(TXB0SIDL, ((frame->ID << 5) & 0xC0) | (frame->Extended == 1 ? 8 : 0) | ((frame->ID >> 27) && 3));
 
-	SPI_set(self, TXB0CTRL, TXREQ_SET | TXP_INTER_HIGH);
+	set(TXB0DLC, (frame->Length & 0xF) | (frame->Remote == 1 ? 0x40 : 0));
+	set(TXB0D0, frame->Data[0]); SPI_set(self, TXB0D1, frame->Data[1]);
+	set(TXB0D2, frame->Data[2]); SPI_set(self, TXB0D3, frame->Data[3]);
+	set(TXB0D4, frame->Data[4]); SPI_set(self, TXB0D5, frame->Data[5]);
+	set(TXB0D6, frame->Data[6]); SPI_set(self, TXB0D7, frame->Data[7]);
+
+	set(TXB0CTRL, TXREQ_SET | TXP_INTER_HIGH);
 
 	//Initiate send
-	SELECT(self->byte(CAN_RTS_TXB0));
+	self->select();
+	self->byte(CAN_RTS_TXB0);
+	self->deselect();
 }
-char MCP2515_latest(const struct MCP2515 * const self, struct CAN_frame * const frame)
+
+char MCP2515_read(const struct MCP2515 * const self, struct CAN_frame * const frame)
 {
+static struct {
+	char ctrl, sidh, sidl, eid8, eid0, dlc;
+	char * D;
+} _;
 unsigned char dummy;
-unsigned char arb;
+unsigned long arb;
 	if (self->irq() == 0)
 		return 0;
+	_.D = frame->Data;
 	// read from correct buffer
-	dummy = SPI_get(self, CANSTAT);
-	if ((dummy & 0x0E) == 0x0C)
-	{	// read RXB0 into frame
-		arb = SPI_get(self, RXB0SIDL);
-		frame->Extended = 0;
-		frame->Remote = 0;
-		frame->ID = SPI_get(self, RXB0SIDH);
-		frame->ID = arb >> 5 | frame->ID << 3;
-		frame->Length = SPI_get(self, RXB0DLC) & 0xF;
-		frame->Data[0] = SPI_get(self, RXB0D0);
-		frame->Data[1] = SPI_get(self, RXB0D1);
-		frame->Data[2] = SPI_get(self, RXB0D2);
-		frame->Data[3] = SPI_get(self, RXB0D3);
-		frame->Data[4] = SPI_get(self, RXB0D4);
-		frame->Data[5] = SPI_get(self, RXB0D5);
-		frame->Data[6] = SPI_get(self, RXB0D6);
-		frame->Data[7] = SPI_get(self, RXB0D7);
-	}
-	else if ((dummy & 0x0E) == 0x0F)
-	{	// read RXB1 into frame
-	}
-	else
+	dummy = get(CANSTAT) >> 1;
+	switch (dummy & 7)
+	{
+	case 6:	// read RXB0 into frame
+		_.ctrl = get(RXB0CTRL);
+		_.sidh = get(RXB0SIDH); _.sidl = get(RXB0SIDL);
+		_.eid8 = get(RXB0EID8); _.eid0 = get(RXB0EID0);
+		_.dlc  = get(RXB0DLC);
+		_.D[0] = get(RXB0D0); _.D[1] = get(RXB0D1);
+		_.D[2] = get(RXB0D2); _.D[3] = get(RXB0D3);
+		_.D[4] = get(RXB0D4); _.D[5] = get(RXB0D5);
+		_.D[6] = get(RXB0D6); _.D[7] = get(RXB0D7);
+		break;
+	case 7:	// read RXB1 into frame
+		_.ctrl = get(RXB1CTRL);
+		_.sidh = get(RXB1SIDH); _.sidl = get(RXB1SIDL);
+		_.eid8 = get(RXB1EID8); _.eid0 = get(RXB1EID0);
+		_.dlc  = get(RXB1DLC);
+		_.D[0] = get(RXB1D0); _.D[1] = get(RXB1D1);
+		_.D[2] = get(RXB1D2); _.D[3] = get(RXB1D3);
+		_.D[4] = get(RXB1D4); _.D[5] = get(RXB1D5);
+		_.D[6] = get(RXB1D6); _.D[7] = get(RXB1D7);
+		break;
+	default:
 		return 0;
+	}
+
+	arb = _.sidl & 3;
+	arb = _.eid8 | (arb << 8);
+	arb = _.eid0 | (arb << 8);
+	arb = _.sidh | (arb << 8);
+	arb = (_.sidl >> 5) | (arb << 3);
+
+	frame->Extended = ((_.sidl  & 8) == 0) ? 0 : 1;
+	frame->Remote = ((_.ctrl & 8) == 0) ? 0 : 1;
+	frame->ID = arb;
+	frame->Length = _.dlc & 0xF;
+
 	// clear SPI CAN interrupts
-	SPI_set(self, CANINTF, 0);
+	set(CANINTF, 0);
 	return 1;
 }
 
-void MCP2515_baudrate(const struct MCP2515 * const self, const unsigned long baud)
+void MCP2515_baud(const struct MCP2515 * const self, const enum CAN_baud baud)
 {
 unsigned char old;
-	old = SPI_get(self, CANCTRL);
-	SPI_set(self, CANCTRL, REQOP_CONFIG);
+	old = get(CANCTRL);
+	set(CANCTRL, REQOP_CONFIG);
 	switch (baud)
 	{
-	case 1000000: case CAN_1Mbps:	SPI_set(self, CNF1, 0); break;
-	case  500000: case CAN_500kbps: SPI_set(self, CNF1, 1); break;
-	case  250000: case CAN_250kbps: SPI_set(self, CNF1, 3); break;
-	case  125000: case CAN_125kbps: SPI_set(self, CNF1, 7); break;
-	case  100000: case CAN_100kbps: SPI_set(self, CNF1, 9); break;
+	case CAN_1Mbps:   set(CNF1, 0); break;
+	case CAN_500kbps: set(CNF1, 1); break;
+	case CAN_250kbps: set(CNF1, 3); break;
+	case CAN_125kbps: set(CNF1, 7); break;
+	case CAN_100kbps: set(CNF1, 9); break;
 	}
-	SPI_set(self, CANCTRL, old);
+	set(CANCTRL, old);
 }
 
 void MCP2515_init(const struct MCP2515 * const self)
 {
-unsigned char dummy;
-	// clear receive masks
-	SPI_set(self, RXM0SIDH, 0x00);
-	SPI_set(self, RXM0SIDL, 0x00);
-
-	SPI_set(self, RXM0EID8, 0x00);
-	SPI_set(self, RXM0EID0, 0x00);
-
-	// clear receve filters
-	SPI_set(self, RXF0SIDL, 0x00);
-
 	// enter config mode
-	SPI_set(self, CANCTRL, REQOP_CONFIG);
+	set(CANCTRL, REQOP_CONFIG);
 
 	// configure bit rate and bit timing
-	SPI_set(self, CNF2, 0x80 | PHSEG1_3TQ | PRSEG_1TQ);
-	SPI_set(self, CNF3, PHSEG2_3TQ);
+	set(CNF2, 0x80 | PHSEG1_3TQ | PRSEG_1TQ);
+	set(CNF3, PHSEG2_3TQ);
 
-	// enable receive interrupt
-	SPI_set(self, CANINTE, RX0IE);
+	// clear receive masks
+	set(RXM0SIDH, 0x00);
+	set(RXM0SIDL, 0x00);
+
+	set(RXM0EID8, 0x00);
+	set(RXM0EID0, 0x00);
+
+	set(RXM1SIDH, 0x00);
+	set(RXM1SIDL, 0x00);
+
+	set(RXM1EID8, 0x00);
+	set(RXM1EID0, 0x00);
+
+	// clear receve filters
+	set(RXF0SIDL, 0x00);
+	set(RXF1SIDL, 0x00);
+
+	// enable receive interrupts, disable all other interrupts
+	set(CANINTE, RX0IE | RX1IE);
 
 	// clear transmit buffers
-	SPI_set(self, TXB0CTRL, 0);
-
-	// return to normal mode
-	SPI_set(self, CANCTRL, REQOP_NORMAL | CLKOUT_ENABLED | OSM_ENABLED);
-
-	// clear transmit buffers
-	SPI_set(self, TXB0CTRL, 0);
+	set(TXB0CTRL, 0);
 
 	//Verify device entered Normal mode
-	dummy = SPI_get(self, CANSTAT);
-	if (OPMODE_NORMAL != (dummy && 0xE0))
-		SPI_set(self, CANCTRL, REQOP_NORMAL | CLKOUT_ENABLED);
-		// What good does it do doing this twice rather than in a while loop? why not OSM_EN again?
+	while (OPMODE_NORMAL != (get(CANSTAT) & 0xE0))
+		set(CANCTRL, REQOP_NORMAL | CLKOUT_ENABLED | OSM_ENABLED);
 
 	// clear receive buffers
-	SPI_set(self, RXB0CTRL, 0x60);
-	SPI_set(self, RXB1CTRL, 0x60);
+	set(RXB0CTRL, 0x60);
+	set(RXB1CTRL, 0x60);
 }
