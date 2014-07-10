71e56e11f6618994a0c787a66a07fb13939421e0
Joe Dowswell <joe@radix2design.co.uk>
2014-07-10 09:56:53 +0100

    [MAX7301] use spi.h

--- lib/MAX7301.c
+++ lib/MAX7301.c
@@ -14,34 +14,36 @@ enum CMD {
 	OUTPUT = 1, INPUT = 2, PULLUP = 3
 };
 
-#define IO_set	self->command
-#define IO_get(cmd)	(self->command(cmd | 0x80, 0), self->command(0x80, 0))
+static char word(spi_pt self, char cmd, char value) { self->select(); self->byte(cmd); value = self->byte(value); self->deselect(); return value; }
+
+#define IO_set(cmd, v)	word(self, cmd, v)
+#define IO_get(cmd)		(word(self, cmd | 0x80, 0), word(self, 0x80, 0))
 
 #define IO_change(cmd, value, mask)	IO_set(cmd, ((value) & (mask)) | (IO_get(cmd) & ~(mask)))
 
-void MAX7301_init(const struct MAX7301 * const self)
+void MAX7301_init(spi_pt self)
 {
 	IO_set(CONF, 1);
 }
 
-void MAX7301_conf(const struct MAX7301 * const self, char p, char conf)
+void MAX7301_conf(spi_pt self, char p, char conf)
 {
 char cmd = PC(p), shift = PCs(p), mask = 3 << shift;
 	shift = (conf & 3) << PCs(p);	// because `<< shift` does not work?
 	IO_change(cmd, shift, mask);
 }
 
-char MAX7301_get(const struct MAX7301 * const self, char p)
+char MAX7301_get(spi_pt self, char p)
 {
 	return IO_get(PORT(p));
 }
 
-void MAX7301_set(const struct MAX7301 * const self, char p, char v)
+void MAX7301_set(spi_pt self, char p, char v)
 {
 	IO_set(PORT(p), v & 1);
 }
 
-void MAX7301_change(const struct MAX7301 * const self, char p, char v, char m)
+void MAX7301_change(spi_pt self, char p, char v, char m)
 {
 	IO_change(P8(p % 32), v, m);
 }
--- lib/MAX7301.h
+++ lib/MAX7301.h
@@ -3,27 +3,19 @@
 */
 #ifndef __MAX7301_H
 #define __MAX7301_H
-
-struct MAX7301 {
-	// exchange a word with the device (including chip selection)
-	char (*command)(char, char);
-};
-
-#define MAX7301_new(name, cs, byte)\
-	static char name##_word(char cmd, char value) { cs = 0; byte(cmd); value = byte(value); cs = 1; return value; }\
-	code const struct MAX7301 name = { name##_word }
+#include "SPI.h"
 
 // start the MAX7301  device
-extern void MAX7301_init(const struct MAX7301 * const);
+extern void MAX7301_init(spi_pt);
 // configure a port pin
 // -- conf(device, port, configuration{1:output,2:input,3:pullup})
-extern void MAX7301_conf(const struct MAX7301 * const, char, char conf);
+extern void MAX7301_conf(spi_pt, char, char conf);
 // get the state of a port pin
-extern char MAX7301_get(const struct MAX7301 * const, char);
+extern char MAX7301_get(spi_pt, char);
 // set the state of a port pin
-extern void MAX7301_set(const struct MAX7301 * const, char, char value);
+extern void MAX7301_set(spi_pt, char, char value);
 // chnange up to 8 port pins
 // - changes port thru port+7 to (value & mask) leaving (~mask) unchanged
-extern void MAX7301_change(const struct MAX7301 * const, char, char value, char mask);
+extern void MAX7301_change(spi_pt, char, char value, char mask);
 
 #endif // __MAX7301_H
