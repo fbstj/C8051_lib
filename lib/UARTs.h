0b3cc6d11fcdc558369f31f0c18644b984752a1b
Joe Dowswell <joe@radix2design.co.uk>
2014-07-10 09:28:21 +0100

    [UART] clean uarts.h

--- lib/UARTs.h
+++ lib/UARTs.h
@@ -7,31 +7,8 @@
 struct UART_MEM { unsigned char buffer[256], head, tail; };
 #define UART_size(len, head, tail)	(((len) + (head) - (tail)) % (len))
 #define UART_clear(r)	(r)->head = (r)->tail = 0
-
-/*
-	UART0 controls
-*/
-extern void U0_init(unsigned long baud);
-// send a string
-extern int U0_puts(char *, int);
-// check for pending characters
-extern int U0_pending();
-// read a byte
-extern int U0_getc();
-
-extern code const struct device U0;
-
-/*
-	UART1 controls
-*/
-extern void U1_init(unsigned long baud);
-// send a string
-extern int U1_puts(char *, int);
-// check for pending characters
-extern int U1_pending();
-// read a byte
-extern int U1_getc();
-
-extern code const struct device U1;
+#define UART_MEM_get(self)	((self)->buffer[(self)->tail++])
+#define UART_MEM_pend(self)	UART_size(sizeof((self)->buffer), (self)->head, (self)->tail)
+#define UART_MEM_puts(self, str, len) { unsigned char i; for (i = 0; i < (len); i++) (self)->buffer[(self)->head++] = (str)[i]; }
 
 #endif	// __UART_H
